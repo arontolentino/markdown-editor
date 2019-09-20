98225484ce7b0ad94edb71d0052fb2cb94dc82a2
Aron Tolentino <arontolentino@outlook.com>
2019-09-20 15:09:09 -0400

    Create initial markdown editor

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,74 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div class="home">
+    <div id="editor">
+      <textarea v-model="input"></textarea>
     </div>
-    <router-view />
+    <div id="preview">
+      <div v-html="compiledMarkdown"></div>
+    </div>
   </div>
 </template>
 
+<script>
+import marked from 'marked';
+
+export default {
+  data: function() {
+    return {
+      input:
+        'Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n '
+    };
+  },
+  computed: {
+    compiledMarkdown: function() {
+      return marked(this.input);
+    }
+  }
+};
+</script>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+html,
+body,
+.home {
+  margin: 0;
+  height: 100vh;
+  width: 100%;
+  font-family: 'Roboto', sans-serif;
+  display: flex;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+#editor,
+#preview {
+  height: 100%;
+  width: 50%;
+  vertical-align: top;
+  box-sizing: border-box;
+  overflow-y: scroll;
+  overflow-x: hidden;
+}
+
+#editor textarea {
+  font-family: 'Monaco', courier, monospace;
+  font-size: 14px;
+  border: none;
+  padding: 20px;
+  outline: none;
+  height: 100%;
+  width: 100%;
+}
+
+#preview {
+  padding: 0 20px;
+  background-color: #f7f7f7;
+}
+
+#preview div {
+  height: 100%;
+}
+
+#preview img {
+  max-width: 100%;
+  display: block;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,69 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <textarea id="editor" v-model="input"></textarea>
+    <div id="preview">
+      <div v-html="compiledMarkdown"></div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import marked from 'marked';
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      input: '# hello'
+    };
+  },
+  computed: {
+    compiledMarkdown: function() {
+      return marked(this.input);
+    }
   }
 };
 </script>
+
+<style>
+html,
+body,
+.home {
+  margin: 0;
+  height: 100vh;
+  width: 100%;
+  font-family: 'Roboto', sans-serif;
+  display: flex;
+}
+
+#editor,
+#preview {
+  width: 50%;
+  vertical-align: top;
+  box-sizing: border-box;
+}
+
+#editor {
+  border: none;
+  font-size: 14px;
+  outline: none;
+  font-family: 'Monaco', courier, monospace;
+  padding: 20px;
+}
+
+#preview {
+  padding: 0 20px;
+  background-color: #f7f7f7;
+  overflow-y: scroll;
+}
+
+#preview div {
+  height: 100%;
+  width: 100%;
+}
+
+#preview img {
+  max-width: 100%;
+  display: block;
+}
+</style>
